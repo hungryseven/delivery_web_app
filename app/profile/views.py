71b92915183b1c1d48b11eda9454b443bca0c358
angry7 <alexandrsem7@gmail.com>
2022-02-10 20:59:15 +0500

    more scripts n cosmetic changes

--- app/profile/views.py
+++ app/profile/views.py
@@ -1,14 +1,12 @@
+from time import strftime
 from flask import jsonify, redirect, url_for, render_template, flash, request, session
 from flask_login import current_user, login_required
 from app import db
 from app.models import Address, Food
 from app.profile import bp as profile_bp
 from app.profile.forms import InfoForm, PasswordForm, AddressForm
-from app.auth.phone_verification import request_verification_token, parse_phone_number
-
-# TODO: Во многих представлениях используются обычные редиректы,
-# вместо которых лучше использовать AJAX. По мере изучения JS
-# добавлять скрипты.
+from app.auth.phone_verification import request_verification_token
+from datetime import timezone, datetime
 
 # Функция-представление страницы профиля с персональными данными
 @profile_bp.route('/info', methods=['GET', 'POST'])
@@ -16,6 +14,7 @@ from app.auth.phone_verification import request_verification_token, parse_phone_
 def info():
     info_form = InfoForm()
     password_form = PasswordForm()
+
     # Так как на странице 2 формы, то проверяем событие нажатия
     # и валидацию в отдельных условиях
     if info_form.submit_info.data and info_form.validate():
@@ -23,15 +22,15 @@ def info():
         current_user.first_name = info_form.first_name.data
         current_user.email = info_form.email.data
         db.session.commit()
-        flash('Данные успешно сохранены')
+        flash('Данные успешно сохранены', category='alert alert-success')
         return redirect(url_for('profile.info'))
     if password_form.submit_password.data and password_form.validate():
         if not current_user.check_password(password_form.current_password.data):
-            flash('Неверный пароль')
+            flash('Неверный пароль', category='alert alert-danger')
             return redirect(url_for('profile.info'))
         current_user.set_password(password_form.new_password.data)
         db.session.commit()
-        flash('Пароль успешно изменен')
+        flash('Пароль успешно изменен', category='alert alert-success')
         return redirect(url_for('profile.info'))
     return render_template('profile/info.html', title='Персональные данные', info_form=info_form, password_form=password_form)
 
@@ -47,45 +46,86 @@ def reset_password():
     return redirect(url_for('auth.verify', next='reset_password_verify', prev=url_for('profile.info')))
 
 # Функция-представление страницы профиля с адресами доставки
-@profile_bp.route('/address', methods=['GET', 'POST'])
+@profile_bp.route('/address')
 @login_required
 def address():
     address_form = AddressForm()
-    # Так как на странице 3 события формы (добавление, редактирование, удаление),
-    # то проверяем нажатие каждого и валидацию (кроме удаления) в отдельных условиях
-    if address_form.submit_address.data and address_form.validate():
+    return render_template('profile/address.html', title='Адрес доставки', address_form=address_form)
+
+# Функция-представление добавления адреса
+@profile_bp.route('/address/add', methods=['POST'])
+@login_required
+def add_address():
+    address_form = AddressForm()
+    if address_form.validate_on_submit():
         address_to_add = Address(
-            street=address_form.street.data,
-            house=address_form.house.data,
-            building=address_form.building.data,
-            entrance=address_form.entrance.data,
-            floor=address_form.floor.data,
-            apartment=address_form.apartment.data,
-            additional_info=address_form.additional_info.data, 
-            user=current_user)
+            street=request.form.get('street'),
+            house=request.form.get('house'),
+            building=request.form.get('building', ''),
+            entrance=request.form.get('entrance', ''),
+            floor=request.form.get('floor', ''),
+            apartment=request.form.get('apartment', ''),
+            additional_info=request.form.get('additional_info', ''), 
+            user_id=current_user.id
+        )
         db.session.add(address_to_add)
         db.session.commit()
-        flash('Адрес доставки успешно сохранен')
-        return redirect(url_for('profile.address'))
-    if address_form.edit_address.data and address_form.validate():
-        address_to_edit = Address.query.get(address_form.address_id.data)
-        address_to_edit.street = address_form.street.data
-        address_to_edit.house = address_form.house.data
-        address_to_edit.building = address_form.building.data
-        address_to_edit.entrance = address_form.entrance.data
-        address_to_edit.floor = address_form.floor.data
-        address_to_edit.apartment = address_form.apartment.data
-        address_to_edit.additional_info = address_form.additional_info.data
+        flash('Адрес доставки успешно сохранен', category='alert alert-success')
+
+        # Возвращаем адрес с текущей страницей, чтобы на нее перейти при успешном запросе
+        return jsonify({'result': 'success', 'address_page': url_for('profile.address')})
+
+    # Возвращаем ошибки валидации полей формы
+    return jsonify({'result': 'error', 'errors': address_form.errors})
+
+# Функция-представление редактирования уже добавленного адреса
+@profile_bp.route('/address/edit', methods=['POST'])
+@login_required
+def edit_address():
+    address_form = AddressForm()
+    address_id = request.form.get('address_id')
+    address_to_edit = Address.query.get_or_404(address_id)
+
+    # Если редактируемый адрес не принадлежит юзеру, то вернет JSON с ошибкой
+    if address_to_edit not in current_user.addresses:
+        return jsonify({'result': 'error'})
+
+    if address_form.validate_on_submit():
+        address_to_edit.street = request.form.get('street')
+        address_to_edit.house = request.form.get('house')
+        address_to_edit.building = request.form.get('building', '')
+        address_to_edit.entrance = request.form.get('entrance', '')
+        address_to_edit.floor = request.form.get('floor', '')
+        address_to_edit.apartment = request.form.get('apartment', '')
+        address_to_edit.additional_info = request.form.get('additional_info', '')
         db.session.commit()
-        flash('Адрес доставки успешно изменен')
-        return redirect(url_for('profile.address'))
-    if address_form.delete_address.data:
-        address_to_delete = Address.query.get(address_form.address_id.data)
-        db.session.delete(address_to_delete)
-        db.session.commit()
-        flash('Адрес доставки успешно удален')
-        return redirect(url_for('profile.address'))
-    return render_template('profile/address.html', title='Адрес доставки', address_form=address_form)
+        flash('Адрес доставки успешно изменен', category='alert alert-success')
+
+        # Возвращаем адрес с текущей страницей, чтобы на нее перейти при успешном запросе
+        return jsonify({'result': 'success', 'address_page': url_for('profile.address')})
+
+    # Возвращаем ошибки валидации полей формы
+    return jsonify({'result': 'error', 'errors': address_form.errors})
+
+# Функция-представление удаления сохраненного адреса
+@profile_bp.route('/address/delete', methods=['POST'])
+@login_required
+def delete_address():
+    address_id = request.form.get('id')
+    address_to_delete = Address.query.get_or_404(address_id)
+
+    # Если удаляемый адрес не принадлежит юзеру, то вернет JSON с ошибкой
+    if address_to_delete not in current_user.addresses:
+        return jsonify({'result': 'error'})
+
+    db.session.delete(address_to_delete)
+    db.session.commit()
+    return jsonify(
+        {
+            'result': 'success', 
+            'flashed': {'text': 'Адрес доставки успешно удален', 'category': 'alert alert-success'}
+        }
+    )
 
 # Функция-представление страницы профиля с избранными позициями
 @profile_bp.route('/favourites')
@@ -95,32 +135,36 @@ def favourites():
     return render_template('profile/favourites.html', title='Избранное', favs=favs)
 
 # Функция-представление добавления позиции/товара в избранное.
-# Если товара с таким id не существует, то выкинет 404 ошибку.
-# Если товар УЖЕ находится в избранном и его пытаются добавить, то вернет JSON с ошибкой
-@profile_bp.route('/favourites/add')
-@login_required
+@profile_bp.route('/favourites/add', methods=['POST'])
 def add_to_favourites():
-    food_id = request.args.get('id')
-    food = Food.query.get_or_404(food_id)
-    if food in current_user.favs:
-        return jsonify({'id': food.id, 'result': 'error'})
-    current_user.favs.append(food)
-    db.session.commit()
-    return jsonify({'id': food.id, 'result': 'success'})
+    if current_user.is_authenticated:
+        food_id = request.form.get('id')
+        food = Food.query.get_or_404(food_id)
 
-# Функция-представление удаления позиции/товара из избранного.
-# Если товара с таким id не существует, то выкинет 404 ошибку.
-# Если товара НЕТ в избранном и его пытаются удалить, то вернет JSON с ошибкой
-@profile_bp.route('/favourites/delete')
+        # Если товар УЖЕ находится в избранном и его пытаются добавить, то вернет JSON с ошибкой
+        if food in current_user.favs:
+            return jsonify({'result': 'error', 'id': food.id})
+
+        current_user.favs.append(food)
+        db.session.commit()
+        return jsonify({'result': 'success', 'id': food.id})
+    flash('Авторизуйтесь для совершения данного действия', category='alert alert-info')
+    return jsonify({'result': 'error', 'login_page': url_for('auth.login')})
+
+# Функция-представление удаления позиции/товара из избранного
+@profile_bp.route('/favourites/delete', methods=['POST'])
 @login_required
 def delete_from_favourites():
-    food_id = request.args.get('id')
+    food_id = request.form.get('id')
     food = Food.query.get_or_404(food_id)
+
+    # Если товара НЕТ в избранном и его пытаются удалить, то вернет JSON с ошибкой
     if food not in current_user.favs:
-        return jsonify({'id': food.id, 'result': 'error'})
+        return jsonify({'result': 'error', 'id': food.id})
+        
     current_user.favs.remove(food)
     db.session.commit()
-    return jsonify({'id': food.id, 'result': 'success'})
+    return jsonify({'result': 'success', 'id': food.id})
 
 # Функция-представление, возвращающая JSON с id товаров/позиций,
 # которые есть в избранном у пользователя
@@ -131,7 +175,9 @@ def get_user_favourites():
     return jsonify(user_favourites)
 
 # Функция-представление страницы профиля историей заказов
-@profile_bp.route('/profile/orders')
+@profile_bp.route('/orders')
 @login_required
 def orders():
-    return render_template('profile/orders.html', title='История заказов')
+    orders = current_user.orders.all()
+    return render_template('profile/orders.html', title='История заказов',
+                            orders=orders, Food=Food, timezone=timezone, strftime=datetime.strftime)
