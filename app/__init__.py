ee43c0f586b014e244c0f0c631f47363d9828959
angry7 <alexandrsem7@gmail.com>
2022-02-15 23:03:56 +0500

    config changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -55,6 +57,25 @@ def create_app(config=Config):
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
+
+    if not app.debug and not app.testing:
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/delivery_web_app.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('delivery_web_app startup')
 
     return app
 
